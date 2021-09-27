3e99474980c10f934c6d430b41310b5b6e6d4d19
Rafal Szczepanik <rafal.szczepanik23@gmail.com>
2021-09-27 12:17:00 +0200

    Add an option to indicate a number

--- src/index.js
+++ src/index.js
@@ -3,20 +3,59 @@ import { rand } from "./utils.js";
 const numberText = document.querySelector(".display-numbers-span");
 const lotteryBtn = document.querySelector(".lottery-btn");
 const ballContainer = document.querySelector(".container");
+const myNumbers = document.querySelector(".choosen-numbers span");
+const popUp = document.querySelector(".pop-up");
+const popUpTxt = document.querySelector(".pop-up span");
+const resetBtn = document.querySelector(".reset-btn");
 
-const choosenNumbers = 6;
+const choosenNumbersLength = 6;
 const loterryArr = [];
 const comparedBalls = [];
 let displaydText = "";
 const ballLength = 49;
+const myChoosenNumbers = [];
 
 const createBalls = () => {
   for (let i = 0; i < ballLength; i++) {
     const myBall = document.createElement("div");
     myBall.classList.add("ball");
+    myBall.dataset.number = `${i + 1}`;
     myBall.innerText = `${i + 1}`;
     ballContainer.appendChild(myBall);
   }
+};
+
+const showResult = () => {
+  console.log(myChoosenNumbers);
+  console.log(loterryArr);
+
+  const resultArr = myChoosenNumbers.filter((number) =>
+    loterryArr.includes(number)
+  );
+  console.log(resultArr);
+  popUpTxt.innerText = String(resultArr.length);
+  popUp.classList.remove("visible");
+};
+
+const chooseOneBall = (ball) => {
+  const myNumber = ball.dataset.number;
+  if (myChoosenNumbers.length >= 6) {
+    alert("You have choosen 6 numbers already");
+    return;
+  }
+
+  if (myChoosenNumbers.indexOf(myNumber) === -1) {
+    myChoosenNumbers.push(myNumber);
+    ball.classList.add("clicked-ball");
+    myNumbers.innerText += ` *${myNumber}*`;
+  }
+};
+
+const chooseYourBalls = () => {
+  const balls = document.querySelectorAll(".ball");
+  balls.forEach((ball) =>
+    ball.addEventListener("click", () => chooseOneBall(ball))
+  );
 };
 
 const addClass = (className, index) => {
@@ -52,16 +91,17 @@ const display = () => {
   txtArr.pop();
   displaydText = txtArr.join(",");
   numberText.textContent = displaydText;
+  showResult();
 };
 
 const chooseLuckyNumbers = () => {
-  for (let i = 0; i < choosenNumbers; i++) {
-    loterryArr.push(rand(1, 49));
+  for (let i = 0; i < choosenNumbersLength; i++) {
+    loterryArr.push(String(rand(1, 49)));
   }
 
   const uniqArr = [...new Set(loterryArr)];
   if (loterryArr.length !== uniqArr.length) {
-    console.log("Arrays are diffrent. Choosing another");
+    console.log("Array has a duplicate. Choosing another");
     loterryArr.length = 0;
     chooseLuckyNumbers();
   }
@@ -72,17 +112,29 @@ const clearData = () => {
   numberText.innerText = "";
   comparedBalls.length = 0;
   loterryArr.length = 0;
+  myChoosenNumbers.length = 0;
+  myNumbers.textContent = "";
+  const balls = document.querySelectorAll(".ball");
+  balls.forEach((ball) => ball.classList.remove("clicked-ball"));
+  popUp.classList.add("visible");
 };
 createBalls();
+chooseYourBalls();
 
 const startLottery = () => {
-  lotteryBtn.classList.add("visible");
-  if (numberText.innerText !== "") {
-    clearData();
+  if (myChoosenNumbers.length < 6) {
+    alert("You have to choose 6 numbers");
+    return;
+  } else {
+    lotteryBtn.classList.add("visible");
+    if (numberText.innerText !== "") {
+      clearData();
+    }
+    chooseLuckyNumbers();
+    setTimeout(display, 4200);
+    setTimeout(compareNumbers, 800);
   }
-  chooseLuckyNumbers();
-  setTimeout(display, 3000);
-  compareNumbers();
 };
 
 lotteryBtn.addEventListener("click", startLottery);
+resetBtn.addEventListener("click", clearData);
